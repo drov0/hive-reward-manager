b0986b03c48571167bd65c61623084e835c51ef4
Martin Lees <drov0@users.noreply.github.com>
2018-09-22 22:51:28 +0200

    Last commit destroyed the example config, restoring now

--- config.js
+++ config.js
@@ -1,12 +1,17 @@
 module.exports =
     {
-        "steempress" : {
+        "howo" : {
             "wif":"activewif",
             "powerup" : true,
             "max_ratio" : 1.3,
             "convert_action" : "powerup", // power up the liquid steem
-            "convert_to_account" : "howo", // to the account howo
-            "reset_power_down" : false // Do not reset the power down every week, if you don't power down and actively earn rewards, no need to set this to true
+            "convert_to_account" : "howo" // to the account howo
         },
-
+        "account2" : {
+            "wif":"activewif",
+            "powerup" : true,
+            "max_ratio" : 1.3,
+            "convert_action" : "put_in_savings", // put in savings the liquid steem
+            "convert_to_account" : "howo" // to the account howo which is another account
+        }// etc
     };
