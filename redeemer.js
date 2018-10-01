2b0e8ba279a7b898c54c3d6801374510e44c4042
Martin Lees <drov0@users.noreply.github.com>
2018-10-01 21:16:38 +0200

    Switch from steem-js to dsteem

--- redeemer.js
+++ redeemer.js
@@ -1,18 +1,10 @@
-var steem = require('steem');
 var randy = require("randy");
-var accounts = require('./config.js')
-const moment = require("moment")
+var accounts = require('./config.js');
+const moment = require("moment");
 
-steem.api.setOptions({url: 'https://api.steemit.com'});
+var dsteem = require('dsteem');
 
-const dsteem = require('dsteem');
-let opts = {};
-//connect to production server
-opts.addressPrefix = 'STM';
-opts.chainId =
-    '0000000000000000000000000000000000000000000000000000000000000000';
-//connect to server which is connected to the network/production
-const client = new dsteem.Client('https://api.steemit.com');
+var client = new dsteem.Client('https://api.steemit.com');
 
 function power_down(account, wif, vesting)
 {
@@ -27,7 +19,7 @@ function power_down(account, wif, vesting)
             },
         ];
         client.broadcast.sendOperations([op], privateKey).then(
-            function(result) {
+            function() {
                 console.log("Power down reset on "+account);
                 return resolve("=");
             },
@@ -59,21 +51,12 @@ function decimalPlaces(num) {
         - (match[2] ? +match[2] : 0));
 }
 
-function getprice()
-{
-    return new Promise(async resolve => {
-        steem.api.getOrderBook(1, function (err, price) {
-            return resolve(price);
-        });
-    });
-}
-
-
 async function sell_sbd(account, reward_sbd, name)
 {
     return new Promise(async resolve => {
-        const price_data = await getprice();
-        const seconds = Math.round(Date.now() / 1000);
+        const price_data = await client.database.call("get_order_book");
+        const seconds = Math.round(Date.now() / 1000) + 604800;
+        const date = (new Date(seconds * 1000)).toISOString().slice(0, 19);
         const price = price_data.asks[0].real_price;
         if (parseFloat(price) <= account['max_ratio']) {
             console.log(reward_sbd + " on the account, price is "+ price +" sbd per steem, selling it ");
@@ -90,11 +73,32 @@ async function sell_sbd(account, reward_sbd, name)
             else
                 sell += " STEEM";
 
-            steem.broadcast.limitOrderCreate(account['wif'], name, randy.getRandBits(32),
-                reward_sbd, sell, false, seconds + 604800, function (err, result) {
+
+            const op = [
+                "limit_order_create",
+                {
+                    amount_to_sell : reward_sbd,
+                    expiration : date,
+                    fill_or_kill : false,
+                    min_to_receive : sell,
+                    orderid : randy.getRandBits(32),
+                    owner : name
+                }
+            ];
+
+            const privateKey = dsteem.PrivateKey.fromString(account['wif']);
+
+            client.broadcast.sendOperations([op], privateKey).then(
+                function() {
                     console.log("sent buy order for " + name + " : " + sell);
                     return resolve("=");
-                });
+                },
+                function(error) {
+                    console.error(error);
+                    return resolve("-");
+                }
+            );
+
         } else
             return resolve("=")
     });
@@ -107,67 +111,79 @@ function wait(time)
     });
 }
 
-function execute(times) {
+async function execute(times) {
     console.log("Execution minute : " + times);
     for (let account in accounts) {
 
-        steem.api.getAccounts([account], async function (err, response) {
-            const reward_sbd = response[0]['reward_sbd_balance']; // will be claimed as Steem Dollars (SBD)
-            const reward_steem = response[0]['reward_steem_balance']; // this parameter is always '0.000 STEEM'
-            const reward_vests = response[0]['reward_vesting_balance']; // this is the actual VESTS that will be claimed as SP
+        let response = await client.database.getAccounts([account]);
+        const reward_sbd = response[0]['reward_sbd_balance']; // will be claimed as Steem Dollars (SBD)
+        const reward_steem = response[0]['reward_steem_balance']; // this parameter is always '0.000 STEEM'
+        const reward_vests = response[0]['reward_vesting_balance']; // this is the actual VESTS that will be claimed as SP
 
-            const name = response[0].name;
+        const name = response[0].name;
 
-            if (accounts[name].reset_power_down === true) {
-                const current_date = moment();
-                const power_down_date = moment(response[0].next_vesting_withdrawal);
-                const duration = moment.duration(power_down_date.diff(current_date));
+        if (accounts[name].reset_power_down === true) {
+            const current_date = moment();
+            const power_down_date = moment(response[0].next_vesting_withdrawal);
+            const duration = moment.duration(power_down_date.diff(current_date));
 
-                if (duration._milliseconds > 0 && accounts[name].power_down_date === undefined) {
-                    accounts[name].power_down_date = response[0].next_vesting_withdrawal;
-                } else if (accounts[name].power_down_date !== undefined && accounts[name].power_down_date !== response[0].next_vesting_withdrawal) {
-                    console.log("reset power down on " + name + "Powering down " + response[0].vesting_shares);
-                    await power_down(name, accounts[name]['wif'], response[0].vesting_shares);
-                    accounts[name].power_down_date = response[0].next_vesting_withdrawal;
+            if (duration._milliseconds > 0 && accounts[name].power_down_date === undefined) {
+                accounts[name].power_down_date = response[0].next_vesting_withdrawal;
+            } else if (accounts[name].power_down_date !== undefined && accounts[name].power_down_date !== response[0].next_vesting_withdrawal) {
+                console.log("reset power down on " + name + "Powering down " + response[0].vesting_shares);
+                await power_down(name, accounts[name]['wif'], response[0].vesting_shares);
+                accounts[name].power_down_date = response[0].next_vesting_withdrawal;
+            }
+        }
+
+        // Triggers every 5 minutes
+        if (times % 5 === 0) {
+            if (accounts[name].convert_sbd === true) {
+                if (parseFloat(response[0].sbd_balance) > 0) {
+                    console.log("Selling sbd and executing actions on it for account : " + name);
+                    await sell_sbd(accounts[name], response[0].sbd_balance, name);
                 }
             }
 
-            // Triggers every 5 minutes
-            if (times % 5 === 0) {
-                console.log("Selling sbd and executing actions on it for account : " + name);
-                if (accounts[name].convert_sbd === true) {
-                    if (parseFloat(response[0].sbd_balance) > 0) {
-                        await sell_sbd(accounts[name], response[0].sbd_balance, name);
-                    }
+            if (accounts[name].liquid_action === "powerup") {
+                if (parseFloat(response[0].balance) > 0) {
+                    power_up(accounts[name]['wif'], name, accounts[name].liquid_to_account, response[0].balance);
+                    console.log(response[0].balance + " on " + name + ", powering it up to " + accounts[name].liquid_to_account)
                 }
-
-                if (accounts[name].liquid_action === "powerup") {
-                    if (parseFloat(response[0].balance) > 0) {
-                        power_up(accounts[name]['wif'], name, accounts[name].liquid_to_account, response[0].balance);
-                        console.log(response[0].balance + " on " + name + ", powering it up to " + accounts[name].liquid_to_account)
-                    }
-                } else if (accounts[name].liquid_action === "put_in_savings") {
-                    if (parseFloat(response[0].balance) > 0) {
-                        transfer_to_savings(accounts[name]['wif'], name, accounts[name].liquid_to_account, response[0].balance);
-                        console.log(response[0].balance + " on " + name + ", putting it in the savings to " + accounts[name].liquid_to_account)
-                    }
+            } else if (accounts[name].liquid_action === "put_in_savings") {
+                if (parseFloat(response[0].balance) > 0) {
+                    transfer_to_savings(accounts[name]['wif'], name, accounts[name].liquid_to_account, response[0].balance);
+                    console.log(response[0].balance + " on " + name + ", putting it in the savings to " + accounts[name].liquid_to_account)
                 }
             }
+        }
 
-            // if it's been an hour since the last execution.
-            if (times === 60) {
-                console.log("Claiming rewards for account : " + name);
-                if (parseFloat(reward_sbd) > 0 || parseFloat(reward_steem) > 0 || parseFloat(reward_vests) > 0) {
-                    steem.broadcast.claimRewardBalance(accounts[name]['wif'], name, reward_steem, reward_sbd, reward_vests, async function () {
-                        console.log(name + " reward : " + reward_sbd + " , " + reward_steem + " " + reward_vests);
-                        if (parseFloat(reward_sbd) > 0) {
-                            await sell_sbd(accounts[name], reward_sbd, name)
-                        }
+        // if it's been an hour since the last execution.
+        if (times === 60) {
+            console.log("Claiming rewards for account : " + name);
+            if (parseFloat(reward_sbd) > 0 || parseFloat(reward_steem) > 0 || parseFloat(reward_vests) > 0) {
 
-                    });
+                const privateKey = dsteem.PrivateKey.fromString(accounts[name]['wif']);
+                const op = [
+                    'claim_reward_balance',
+                    {
+                        account: name,
+                        reward_steem: reward_steem,
+                        reward_sbd: reward_sbd,
+                        reward_vests: reward_vests,
+                    },
+                ];
+                await client.broadcast.sendOperations([op], privateKey).catch( function(error) {
+                    console.error(error);
+                });
+
+                console.log(name + " reward : " + reward_sbd + " , " + reward_steem + " " + reward_vests);
+                if (parseFloat(reward_sbd) > 0) {
+                    await sell_sbd(accounts[name], reward_sbd, name)
                 }
             }
-        });
+        }
+
     }
 }
 
@@ -190,15 +206,49 @@ run();
 
 
 function power_up(Activekey, from, to, amount) {
-    steem.broadcast.transferToVesting(Activekey, from, to, amount, "",function(err, result) {
 
-    });
+    const privateKey = dsteem.PrivateKey.fromString(Activekey);
+
+    const op = [
+        'transfer_to_vesting',
+        {
+            from: from,
+            to: to,
+            amount: amount,
+        },
+    ];
+    client.broadcast.sendOperations([op], privateKey).then(
+        function(result) {
+
+        },
+        function(error) {
+        }
+    );
 }
 
 function transfer_to_savings(Activekey, from, to, amount) {
-    steem.broadcast.transferToSavings(Activekey, from, to, amount, "",function(err, result) {
 
-    });
+    const privateKey = dsteem.PrivateKey.fromString(Activekey);
+
+    const op = [
+        'transfer_to_savings',
+        {
+            from: from,
+            to: to,
+            amount: amount,
+            memo : "",
+            request_id : randy.getRandBits(32),
+
+        },
+    ];
+    client.broadcast.sendOperations([op], privateKey).then(
+        function(result) {
+
+        },
+        function(error) {
+        }
+    );
+
 }
 
 
